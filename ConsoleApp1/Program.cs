b4fb9f0fb9f34398c01d860f32625ecfbb3453e8
HamletPB <shaolinpb@gmail.com>
2022-08-26 11:54:58 -0300

    deixando as funcoes mais "elegantes"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,43 +18,32 @@ namespace Salario
             sal =  Convert.ToDouble(Console.ReadLine());
            
 
-            Console.WriteLine("digite H para o salario por hora");
-            Console.WriteLine("digite D para o salario por dia");
-            Console.WriteLine("digite A para o salario por ano");
+            Console.WriteLine("digite H para saber o salario por hora");
+            Console.WriteLine("digite D para saber o salario por dia");
+            Console.WriteLine("digite A para saber o salario por ano");
 
-            switch (Console.ReadLine())
+            switch (Console.ReadLine().ToUpper())
 {
                 case "H":
-                    Console.WriteLine("o salario por hora e: " + hora(sal) );
-                    // código 1
+                    Console.WriteLine("o salario por hora e: " + hora(sal) );                 
                     break;
+
                 case "D":
-                    Console.WriteLine("o salario por dia e: " + dia(sal) );
-                    // código 2
+                    Console.WriteLine("o salario por dia e: " + dia(sal) );                    
                     break;
 
                 case "A":
-                    Console.WriteLine("o salario por ano e: " + ano(sal) );
-                    // código 4
+                    Console.WriteLine("o salario por ano e: " + ano(sal) );                    
                     break;
             }
-
             Console.ReadLine();
             
         }
-       public static double dia(double salMes)
-        {
-            return salMes / 20;
-        }
-
-       public static double hora(double salMes)
-        {
-            return salMes / (8 * 20);
-        }
-
-       public static double ano(double salMes)
-        {
-            return salMes * 12;
-        }
+       public static double dia(double salMes)  => salMes / 20;
+       
+       public static double hora(double salMes) => salMes / (160);
+        
+       public static double ano(double salMes)  => salMes * 12;
+   
     }
 }
